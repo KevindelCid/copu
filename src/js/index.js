3d00544503d720e418662a6791f9733d217b68b0
kevin del cid <danieldelcidf@gmail.com>
2024-09-30 09:22:33 -0600

    add input

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,8 @@
 function copyTextToClipboard(text) {
-  // Crea un elemento de texto temporal para seleccionar el contenido
   const tempInput = document.createElement("textarea");
   tempInput.value = text;
   document.body.appendChild(tempInput);
-
-  // Seleccionar y copiar el texto
   tempInput.select();
   document.execCommand("copy");
-
-  // Eliminar el elemento temporal
   document.body.removeChild(tempInput);
-
-  alert("Texto copiado al portapapeles!");
 }
