ebde6f0eafdab4e61b569f93ff09feaaab8ee35b
kevin del cid <danieldelcidf@gmail.com>
2024-09-30 10:13:11 -0600

    add share option whit sharejs

--- src/js/index.js
+++ src/js/index.js
@@ -7,23 +7,20 @@ function copyTextToClipboard(text) {
   document.body.removeChild(tempInput);
 }
 
+function isWebShareSupported() {
+  return !!navigator.share;
+}
+
 function shareText(text) {
-  // Verificar si el navegador soporta la Web Share API
-  if (navigator.share) {
-    navigator
-      .share({
-        title: "Compartir texto",
-        text: text, // El texto que deseas compartir
-      })
-      .then(() => {
-        console.log("Texto compartido con éxito!");
-      })
-      .catch((error) => {
-        console.error("Error al compartir:", error);
-      });
-  } else {
-    alert(
-      "La API de compartir no es compatible con este navegador o versión de WebView."
-    );
-  }
+  navigator
+    .share({
+      title: "Compartir texto",
+      text: text,
+    })
+    .then(() => {
+      console.log("Texto compartido con éxito!");
+    })
+    .catch((error) => {
+      console.error("Error al compartir:", error);
+    });
 }
